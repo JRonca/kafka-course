0b4aee988fc4ab2b63c9a1d7c2d1a11a0e41ce39
JRonca <jrfr21112000@gmail.com>
2024-05-22 08:26:34 -0300

    Error Handler Details

--- str-consumer/src/main/java/com/jronca/strconsumer/exceptions/ErrorCustomHandler.java
+++ str-consumer/src/main/java/com/jronca/strconsumer/exceptions/ErrorCustomHandler.java
@@ -12,7 +12,11 @@ import org.springframework.stereotype.Component;
 public class ErrorCustomHandler implements KafkaListenerErrorHandler {
     @Override
     public Object handleError(Message<?> message, ListenerExecutionFailedException exception) {
-    log.info("EXCEPTION_HANDLER ::: Capturei um erro");
+        log.info("EXCEPTION_HANDLER ::: Capturei um erro");
+        log.info("Payload ::: {}", message.getPayload());
+        log.info("Offset ::: {}", message.getHeaders());
+        log.info("Offset ::: {}", message.getHeaders().get("kafka_offset"));
+        log.info("Message Exception ::: {}", exception.getMessage());
         return null;
     }
 
