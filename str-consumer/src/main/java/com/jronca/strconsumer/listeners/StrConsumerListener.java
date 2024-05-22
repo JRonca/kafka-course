e45162daee565a9120af48fa13d94ad0b61c9d1d
JRonca <jrfr21112000@gmail.com>
2024-05-22 08:05:53 -0300

    Error Handler

--- str-consumer/src/main/java/com/jronca/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/jronca/strconsumer/listeners/StrConsumerListener.java
@@ -1,27 +1,29 @@
 package com.jronca.strconsumer.listeners;
 
 import com.jronca.strconsumer.custom.StrConsumerCustomListener;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
+    @SneakyThrows
     @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message){
-        log.info("0 ::: Receive message {}", message);
+        log.info("CREATE ::: Receive message {}", message);
+        throw new IllegalArgumentException("Exception...");
     }
 
     @StrConsumerCustomListener(groupId = "group-1")
-    public void listener(String message){
-        log.info("1 ::: Receive message {}", message);
+    public void log(String message){
+        log.info("LOG ::: Receive message {}", message);
     }
 
     @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
-    public void listenerGroup2(String message){
-        log.info("2 ::: Receive message {}", message);
+    public void history(String message){
+        log.info("HISTORY ::: Receive message {}", message);
     }
 }
