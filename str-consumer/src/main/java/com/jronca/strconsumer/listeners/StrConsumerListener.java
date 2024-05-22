7942c90f11c67b89fd739abe954845d718528502
JRonca <jrfr21112000@gmail.com>
2024-05-21 21:09:05 -0300

    Record Interceptor

--- str-consumer/src/main/java/com/jronca/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/jronca/strconsumer/listeners/StrConsumerListener.java
@@ -20,7 +20,7 @@ public class StrConsumerListener {
         log.info("1 ::: Receive message {}", message);
     }
 
-    @StrConsumerCustomListener(groupId = "group-2")
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
     public void listenerGroup2(String message){
         log.info("2 ::: Receive message {}", message);
     }
