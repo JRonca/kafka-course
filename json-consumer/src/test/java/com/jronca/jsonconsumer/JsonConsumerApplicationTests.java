e44cc1954dfd8408f4139ef48d44faa57a93edaa
JRonca <jrfr21112000@gmail.com>
2024-05-23 08:49:03 -0300

    Add Json Consumer

---
+++ json-consumer/src/test/java/com/jronca/jsonconsumer/JsonConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jronca.jsonconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
